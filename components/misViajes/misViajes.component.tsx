ee4c0349b3af061fe1eab8432f991ec5fb848334
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-03-15 21:17:46 +0100

    feat: estructura de la pagina

---
+++ components/misViajes/misViajes.component.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import style from './misViajes.module.scss';
+import { IconChevronsRight } from '@tabler/icons-react';
+
+export default function MisViajesComponent() {
+
+    return (
+        <article className={style.component_container}>
+            <Image
+                width={250}
+                height={250}
+                src={'/images/sello.png'}
+                alt={'Foto de sello'}
+            />
+            <Image
+                width={500}
+                height={700}
+                src={'/images/perfil_viajera.jpg'}
+                alt={'Foto perfil viajera'}
+            />
+            <div>
+                <h1>MIS VIAJES</h1>
+                <p>
+                    Soy Nuria Zamora, manchega y villamalense desde 1994, enamorada de mi tierra, la Manchuela, aunque durante los últimos 10 años he estado viviendo en Albacete, la cuidad que me ha visto crecer profesionalmente y a la que tengo grandes cosas que agradecer.
+                </p>
+                <p>
+                    Graduada en Administración y Dirección de Empresas en el año 2018 por la Universidad de Castilla-La Mancha. De esta etapa de mi vida, resalto el esfuerzo y sacrificio, además de las grandes amistades que logré conseguir ahí. En mi último año de carrera, tuve la oportunidad de especializarme en Dirección Estratégica y Marketing. Gracias a esta decisión, descubrí cuál era mi verdadera pasión y cuál era el ámbito al que me quería dedicar el resto de mi vida, “EL MARKETING”.
+                </p>
+                <p>
+                    Continué mi andadura como estudiante de máster en la Universidad Internacional de La Rioja formándome como directora de Marketing Estratégico y, ¡esto no acaba aquí! Actualmente, me encuentro especializándome en Marketing Digital dentro de la misma Universidad.
+                </p>
+                <p>
+                    El inglés es un idioma que domino con Certificado de B1 Preliminary Cambrigde, aunque sigo en constante aprendizaje y formación.
+                </p>
+                <p>
+                    <span className={style.chip}>#estrategia</span>
+                    <span className={style.chip}>#eventos</span>
+                    <span className={style.chip}>#marketing_digital</span>
+                    <span className={style.chip}>#comunicación</span>
+                    <span className={style.chip}>#campañas</span>
+                </p>
+            </div>
+            <IconChevronsRight color='rgb(67, 195, 255)' size={64}/>
+        </article>
+    )
+}
---
+++ containers/index/index.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import style from './index.module.scss';
+import PerfilDeViajeraComponent from '../../components/perfilDeViajera/perfilDeViajera.component';
+import MisViajesComponent from '../../components/misViajes/misViajes.component';
+
+export default function IndexContainer() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [popUpSelected, setPopUpSelected] = useState('');
+
+    return (
+        <section>
+            {popUpSelected.length > 0 &&
+
+                <div className={`${style.popup_container}`}>
+                    {popUpSelected === 'perfilDeViajera' &&
+                        <PerfilDeViajeraComponent next={() => setPopUpSelected('misViajes')} />
+                    }
+                    {popUpSelected === 'misViajes' &&
+                        <MisViajesComponent />
+                    }
+                </div>
+            }
+            <article className={style.presentation_container}>
+                <div className={style.background_image}></div>
+                <div className={style.button_menu_navigation_container}>
+                    <div
+                        className={menuOpen ? style.button_menu_navigation_open : style.button_menu_navigation_closed}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <header className={menuOpen ? style.header_open : style.header_closed}>
+                    <ul>
+                        <li onClick={() => setPopUpSelected('perfilDeViajera')} className={popUpSelected === 'perfilDeViajera' ?style.menu_link_selected : null}>Perfil de viajera</li>
+                        <li onClick={() => setPopUpSelected('misViajes')} className={popUpSelected === 'misViajes' ?style.menu_link_selected : null}>Mis viajes</li>
+                        <li>Eventos y campañas</li>
+                        <li>Habilidades</li>
+                        <li>Compañeros de viaje</li>
+                    </ul>
+                </header>
+                <div className={style.text_presentation_container}>
+                    <h1>¡Hola! ¿Quieres viajar conmigo?</h1>
+                    <span>Acompáñame en este viaje y descubre todas mis experiencias</span>
+                    <span>#marketing - #eventos</span>
+                    <article className={style.buttons_presentation_container}>
+                        <div>¿Hablamos?</div>
+                    </article>
+                </div>
+            </article>
+        </section >
+    )
+}

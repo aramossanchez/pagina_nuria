23f3ff4d4fa8326284d2d53318c1bf6cbdc81b5d
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-04-05 00:46:02 +0200

    fix: linting error

--- components/loader/loader.component.tsx
+++ components/loader/loader.component.tsx
@@ -1,9 +1,9 @@
-import style from './loader.module.scss'
+import style from './loader.module.scss';
 
 export default function LoaderComponent() {
-    return (
-        <div className={style.loader_container}>
-            <div className={style.lds_spinner}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-        </div>
-    )
+  return (
+    <div className={style.loader_container}>
+      <div className={style.lds_spinner}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+  );
 }
